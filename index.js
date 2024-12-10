7ebb17a1625fec00adebee1e44d721d189a9c044
Vishnu Lal <164046416+vishnu-arU@users.noreply.github.com>
2024-12-10 21:01:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ function change() {
   var audio = new Audio("./audio/song.mp3");
   audio.play();
 
-  //var video = document.createElement("video");
-  //video.src = "./video/video.mp4";
-  //video.controls = true;
-  //video.style.width = "50%";
-  //document.body.appendChild(video);
-  //video.play();
-  //   alert("Miss you baby 😘");
+  var video = document.createElement("video");
+  video.src = "./video/video.mp4";
+  video.controls = true;
+  video.style.width = "50%";
+  document.body.appendChild(video);
+  video.play();
+    alert("Miss you baby 😘");
 
-  // video.addEventListener("ended", function () {
-  //   alert("Miss you baby 😘"); // Show the alert after the video finishes
-  // });
-
-  audio.addEventListener("ended", function () {
-    alert("Miss you baby(Chullikambee😘)"); // Show the alert after the video finishes
+  video.addEventListener("ended", function () {
+    alert("Miss you baby 😘"); 
   });
+
+  //audio.addEventListener("ended", function () {
+    //alert("Miss you baby(Chullikambee😘)"); 
+  //});
 }
